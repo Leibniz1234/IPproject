609766d976a75772bf637c1687a17d9f954014dd
Leibniz1234 <51380951+Leibniz1234@users.noreply.github.com>
2022-09-01 18:37:47 +0200

    Add files via upload

--- fuse_signals.py
+++ fuse_signals.py
@@ -26,19 +26,17 @@ class FuseSignal:
 
     def particle_filter(self, filter, ini_pos):
         # filter = kf.ConstantVelocityKalmanFilter()
-        kalman = kalman_filters.ConstantVelocityKalmanFilter(state_dim=6,obs_dim=6)
+        kalman = kalman_filters.ConstantVelocityKalmanFilter(state_dim=6, obs_dim=6)
         fused_signal = [ini_pos[0:3]]
         filter.initialize_filter(ini_pos[0:3])
         kalman.initialize_filter(ini_pos)
         time_step = filter.dt
-        video_time = int((self.timestamps[0] * len(self.signals[0])) / time_step)
+        video_time = int((self.timestamps[0] * self.signals.shape[0]) / time_step)
         for i in range(1, video_time):
+            # fused_signal.append(kalman.predict()[0:3])
             fused_signal.append(filter.predict())
             # TODO: at each timestep -> update kalman for concatenated signals (xyz,xyz) -> predict kalman w/ dt as timestamp -> run particle filter w/ velocity from predicted kalman
-
-            # for j in range(len(self.signals)):
-            #     multiple = (i - 1) * time_step / self.timestamps[j]
-            #     if multiple - int(multiple) == 0 and self.signals[j]:
-            #         filter.update(self.signals[j][int(multiple)], np.array(kalman.X))
-            #         kalman.update(self.signals[j][int(multiple)])
+            kalman.update(self.signals[i])
+            pred = kalman.predict()
+            filter.update(self.signals[i][0:3], pred)
         return fused_signal
